2862bdc7d7ec085e3f5e460adea1d1db008173a4
oyvvl <940709@gmail.com>
2019-10-25 02:23:03 +0900

    fixed typo in cloud domain address

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 const localIPAddress = '192.168.0.47'
-const cloudServerDomain = 'http://api.thingdex.space'
+const cloudServerDomain = 'https://api.thingdex.space'
 const localServerDomain = `http://${localIPAddress}:4000`
 
 const domain = process.env.REACT_APP_NODE_ENV === 'development' ? localServerDomain : cloudServerDomain;
