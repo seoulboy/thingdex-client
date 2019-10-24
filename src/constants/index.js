4cb67d2d765d9778016bec21357815d16a80c236
oyvvl <940709@gmail.com>
2019-10-25 02:20:06 +0900

    modified cloud server domain address

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 const localIPAddress = '192.168.0.47'
-const cloudDomain = 'http://thingdex-env.v3jdr8esif.ap-northeast-2.elasticbeanstalk.com'
-const localDomain = `http://${localIPAddress}:4000`
+const cloudServerDomain = 'http://api.thingdex.space'
+const localServerDomain = `http://${localIPAddress}:4000`
 
-const domain = process.env.REACT_APP_NODE_ENV === 'development' ? localDomain : cloudDomain;
+const domain = process.env.REACT_APP_NODE_ENV === 'development' ? localServerDomain : cloudServerDomain;
 
 module.exports = { domain };
